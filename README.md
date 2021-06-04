dbda63b8df0cda556dd559fda923935d3686ca97
Scott Cyrus Jafary <scottcyrusj@gmail.com>
2021-06-04 10:42:31 +0700

    Initial commit, API working

---
+++ README.md
@@ -0,0 +1,2 @@
+# Uses News API with Axios to fetch the top headlins from The Verge!
+Experience using an API with Axios to fetch data and render it with EJS.
---
+++ app.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path')
+const axios = require('axios');
+const app = express();
+const apiKey = process.env.API_KEY;
+
+//set the directory for all templating engine files (ejs)
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  axios.get(`https://newsapi.org/v2/top-headlines?sources=the-verge&apiKey=${apiKey}`)
+    .then(({test}) => {
+      console.log(test.data.articles[0].title)
+      const articleTitle = test.data.articles[0].title;  
+      res.render('home.ejs', {articleTitle});  
+  })
+  .catch(err => {
+    console.log("oops!", err)
+  }) 
+}) 
+
+
+// const getApiData = async () => {
+//   const articles = await axios.get(`https://newsapi.org/v2/top-headlines?sources=the-verge&apiKey=${apiKey}`)
+//   console.log(articles.data.articles)
+//   const newLI = document.createElement('LI');
+//   newLI.append(articles)
+//   list.append(newLI)
+
+// }
+
+// getApiData()
+
+app.get('/', (req, res) => {
+  res.render('home.ejs');
+}) 
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`serving on port ${port}!`)
+})
